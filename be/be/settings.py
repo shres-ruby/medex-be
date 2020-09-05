d9e75f8036cf877ddafe1410750fe76b0c8b2975
shres-ruby <shres.ruby@gmail.com>
2020-09-05 22:14:21 +0545

    refactor: add django heroku

--- be/be/settings.py
+++ be/be/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 
 from pathlib import Path
 from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -208,3 +210,6 @@ LOGGING = {
         },
     }
 }
+
+
+django_heroku.settings(locals())
