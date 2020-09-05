4fd7aa3a3ba09c56241d261e98607d7e23738ce0
shres-ruby <shres.ruby@gmail.com>
2020-09-05 21:46:31 +0545

    refactor: add static root

--- be/be/settings.py
+++ be/be/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
 MEDIA_URL = '/files/'
 
